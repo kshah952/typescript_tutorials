108639932909b9a18a01b12e6a867b4d07f15969
kshah952 <kshah952@rku.ac.in>
2022-02-08 09:37:30 +0530

    Typescript Demo testing Code updated

--- demo.ts
+++ demo.ts
@@ -2,9 +2,20 @@ var msg : string
 msg = "Hello"
 console.log(msg)
 
-var msg: string
+var msg: string //var allow to declare same var in same block
 msg = "Hii"
 console.log(msg)
 
 const arr = [1,2,3]
 console.log(arr)
+
+arr = [1,2,3] //const do not allow to redeclare the array it only allows to change the value
+console.log(arr)
+
+let msg1 : string 
+msg1 = "hii"
+console.log(msg1)
+
+let msg1 : string //let do not allow to declare same var in same block
+msg1 = "hello"
+console.log(msg1)
