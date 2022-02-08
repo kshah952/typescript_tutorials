d572be5719699b178c7a0549f7f4b7a89fd49d58
kshah952 <kshah952@rku.ac.in>
2022-02-08 09:32:37 +0530

    Typescript Demo testing Code

---
+++ demo.js
@@ -0,0 +1,8 @@
+var msg;
+msg = "Hello";
+console.log(msg);
+var msg;
+msg = "Hii";
+console.log(msg);
+var arr = [1, 2, 3];
+console.log(arr);
---
+++ demo.ts
@@ -0,0 +1,10 @@
+var msg : string
+msg = "Hello"
+console.log(msg)
+
+var msg: string
+msg = "Hii"
+console.log(msg)
+
+const arr = [1,2,3]
+console.log(arr)
